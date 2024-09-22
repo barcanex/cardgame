39b971481c727c462a807da016711e7a83f751a2
barcanex <christos.apollwn97@gmail.com>
2024-09-22 12:03:35 +0300

    Update card_game.py

--- card_game.py
+++ card_game.py
@@ -98,4 +98,31 @@ while game_on:
     player_two_cards = []
     player_one_cards.append(player_one.remove_one())
 
-    print("helo")
+    at_war = True
+    #Comparing cards to see if P1 won.
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+        if player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+        else:
+
+            print("War!")
+            if len(player_one.all_cards) < 5:
+                print("Player 1 unable to declare war")
+                print("Player 2 wins!")
+                game_on = False
+                break
+            elif len(player_two.all_cards) < 5:
+                print("Player 2 unable to declare war!")
+                print("Player 1 wins!")
+                game_on = False
+                break
+    else:
+        for num in range(5):
+            player_one_cards.append(player_one.remove_one())
+            player_two_cards.append(player_two_cards.remove_one())
